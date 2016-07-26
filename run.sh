0f998da91620d209f0def4526dce2bc6c9747956
megastef <stefan.thies@sematext.com>
2016-07-26 12:41:42 +0200

    update to SPM client 1.31, has open issues

--- run.sh
+++ run.sh
@@ -18,14 +18,7 @@ if [ -n "$SPM_CONFIG" ]; then
 fi
 
 IFS=$IFS_ORIGINAL
-
-/etc/init.d/collectd start
-/etc/init.d/spm-sender start
-if [  "$SPM_STANDALONE_MONITOR" == "disabled" ]; then
-	echo "Standalone monitor is disabled"
-else
-	/etc/init.d/spm-monitor start
-	tail -F /opt/spm/spm-monitor/logs/spm-monitor.log | grep -v INFO &
-fi
-tail -F /opt/spm/spm-sender/logs/spm-sender.log | grep -v INFO
+bash /opt/spm/bin/spm-client-setup-os-conf.sh
+/etc/init.d/spm-monitor start
+tail -F /opt/spm/spm-monitor/logs/*/*.log | grep -v INFO
 
