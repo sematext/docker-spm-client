a076e0adc30a858409e4f25c0d7008329faa94dc
megastef <stefan.thies@sematext.com>
2016-08-03 22:47:22 +0200

    add nodejs based agents, use docker-info.js instead of curl

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,23 @@
 FROM debian:jessie
 RUN echo "deb http://http.debian.net/debian jessie-backports main" >> /etc/apt/sources.list && \
-    apt-get -qqy update && apt-get install -qqy socat sudo wget curl cron ntp python openjdk-8-jre-headless jq sysvinit-core sysvinit-utils && \
+    apt-get -qqy update && apt-get install -qqy socat sudo wget curl cron ntp python openjdk-8-jre-headless jq sysvinit-core sysvinit-utils build-essential && \
     apt-get remove --purge --auto-remove systemd -y && \
-    apt-get upgrade -qqy 
-RUN echo "deb http://pub-repo.sematext.com/debian sematext main" >> /etc/apt/sources.list && \
+    curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash - && \
+    echo "deb http://pub-repo.sematext.com/debian sematext main" >> /etc/apt/sources.list && \
 	wget -O - https://pub-repo.sematext.com/debian/sematext.gpg.key |  apt-key add - && \
     apt-get update -qqy && \
-    apt-get install --force-yes -qqy spm-client && \
-    apt-get autoremove && apt-get autoclean
+    apt-get install --force-yes -qqy spm-client nodejs && \
+    apt-get autoremove && apt-get autoclean && \
+    npm i spm-agent-mongodb sematext-agent-httpd sematext-agent-nginx -g && npm i dockerode
 ADD ./run.sh /run.sh
+ADD ./docker-info.js  /tmp/di/docker-info.js
+ADD ./package.json  /tmp/di//package.json
 ADD ./netmap.sh /opt/spm/bin/netmap.sh
 RUN chmod +x run.sh && chmod +x /opt/spm/bin/netmap.sh && \
     chmod +x /opt/spm/bin/spm-client-setup-conf.sh && \
-    ln /usr/bin/env /bin/env
+    chmod +x /tmp/di/docker-info.js && \
+    ln /usr/bin/env /bin/env && \
+    cd /tmp/di/ && npm i  -g && rm -rf /tmp/di
 ENV PATH ${PATH}:/opt/spm/bin/
 VOLUME /opt/spm
 CMD ["/run.sh"]
--- run.sh
+++ run.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 # Configure SPM 
+export STANDALONE_LOG_DIR=/opt/spm/spm-monitor/logs/standalone/
 function get_docker_info () 
 {
 	export DOCKER_SOCKET=/var/run/docker.sock
 	if test -r $DOCKER_SOCKET; then
-		socat TCP-LISTEN:2375,reuseaddr,fork UNIX:/var/run/docker.sock & 
-		export SOCAT_PID=$!
-		sleep 1
-		export DOCKER_INFO=$(curl --silent http://localhost:2375/info)
-		sleep 1
-		kill ${SOCAT_PID} > /dev/null
-		echo "docker_id=$(echo ${DOCKER_INFO} | jq '.ID' | sed s/\"//g)" > /opt/spm/.docker
-		echo "docker_hostname=$(echo ${DOCKER_INFO} | jq '.Name' | sed s/\"//g)" >> /opt/spm/.docker
+		echo "docker_id=$(echo $(docker-info ID))" > /opt/spm/.docker
+		echo "docker_hostname=$(echo $(docker-info Name))" >> /opt/spm/.docker
 		chmod 555 /opt/spm/.docker
 		echo content of /opt/spm/.docker:
 		cat /opt/spm/.docker
@@ -19,10 +14,63 @@ function get_docker_info ()
 		echo "Docker Socket $DOCKER_SOCKET is not readable!"
 		echo "Please add -v ${DOCKER_SOCKET}:${DOCKER_SOCKET} to the docker run command"
 		echo "This will let SPM agents collect the docker hostname to tag metrics."
-		exit -1
+		#exit -1
 	fi
 }
+
+function setup_httpd_agent () 
+{
+	IFS_ORIGINAL=$IFS
+	IFS=' ' read -r -a array <<< "$1"	
+	echo "setup httpd " ${array[0]} ${array[2]} 
+	export SPM_TOKEN=${array[0]}
+	export spmagent_httpd__url=${array[2]}
+	sematext-agent-httpd ${array[0]} ${array[2]}  > $STANDALONE_LOG_DIR/spm-monitor-${array[1]}-config-${array[0]}-default.log & 
+	unset SPM_TOKEN
+	unset spmagent_nginx__url
+	IFS=$IFS_ORIGINAL
+}
+
+function setup_nginx_agent () 
+{
+	IFS_ORIGINAL=$IFS
+	IFS=' ' read -r -a array <<< "$1"	
+	echo "setup nginx " ${array[0]} ${array[2]} 
+	export SPM_TOKEN=${array[0]}
+	export spmagent_nginx__url=${array[2]}
+	sematext-agent-nginx ${array[0]} ${array[2]}  > $STANDALONE_LOG_DIR/spm-monitor-${array[1]}-config-${array[0]}-default.log &
+	unset SPM_TOKEN
+	unset spmagent_nginx__url
+	IFS=$IFS_ORIGINAL
+}
+
+function setup_mongodb_agent()
+{
+	IFS_ORIGINAL=$IFS
+	IFS=' ' read -r -a array <<< "$1"	
+	echo "setup mongodb token="${array[0]} "url="${array[2]} 
+	export spmagent_mongodb__url=${array[2]}
+	export SPM_TOKEN=${array[0]}
+	spm-agent-mongodb ${array[0]} ${array[2]}  > $STANDALONE_LOG_DIR/spm-monitor-${array[1]}-config-${array[0]}-default.log   & 
+	IFS=$IFS_ORIGINAL
+}
+
 function spm_client_setup () 
+{
+	echo "spm_config_setup() $1"
+	case "$1" in
+	  *standalone* ) export SPM_STANDALONE_MONITOR="enabled"; echo "Standalone Monitor enabled for: $cfg";;
+	esac
+	export SPM_LOG_TO_CONSOLE=true
+	case "$1" in
+		*mongodb* ) setup_mongodb_agent "$1";;
+		*httpd* ) setup_httpd_agent "$1";;
+		*nginx* ) setup_nginx_agent "$1";;
+		*) bash -c "/opt/spm/bin/spm-client-setup-conf.sh ${1}";;
+	esac 
+}
+
+function spm_client_setups () 
 {
 	#Set the field separator to new line
 	IFS_ORIGINAL=$IFS
@@ -33,11 +81,9 @@ function spm_client_setup ()
 		do
 		  set IFS = $IFS_ORIGINAL	
 		  echo "-" "$cfg"  
-		  bash -c "/opt/spm/bin/spm-client-setup-conf.sh $cfg"
+		  spm_client_setup "$cfg"
+		  # bash -c "/opt/spm/bin/spm-client-setup-conf.sh $cfg"
 		  # Check for standalone monitors
-		  case "$cfg" in
-		    *standalone* ) export SPM_STANDALONE_MONITOR="enabled"; echo "Standalone Monitor enabled for: $cfg";;
-		  esac
 		done
 	fi
     #sed -i s/spm_sender_hostname_alias=$/spm_sender_hostname_alias=${HOSTNAME}/g /opt/spm/properties/spm-sender.properties
@@ -45,7 +91,7 @@ function spm_client_setup ()
 }
 
 get_docker_info
-spm_client_setup 
+spm_client_setups 
 
 /etc/init.d/spm-monitor restart
 # /bin/bash /opt/spm/spm-monitor/bin/spm-monitor-starter.sh /opt/spm/spm-monitor/conf/spm-monitor-os-config.properties —daemon & 
